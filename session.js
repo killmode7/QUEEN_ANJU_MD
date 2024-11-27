5545fcca1a5fa0a8979e51e77e2c4bcd5a114f9f
killmode7 <stanleykaguu7@gmail.com>
2024-11-27 13:37:45 +0300

    Update session.js

--- session.js
+++ session.js
@@ -21,5 +21,5 @@ function convertToBool(text, fault = 'true') {
     return text === fault ? true : false;
 }
 module.exports = {
-SESSION_ID: process.env.SESSION_ID || "your session_id here",
+SESSION_ID: process.env.SESSION_ID || "ANJU-MD=7I9A3ayB#xjA-1INkD_MX038VndyTDIf2PSPsyirKcopv_onkO0E",
 };
