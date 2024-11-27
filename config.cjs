3cc98bae98e95a907293d6cb9c6f7fd1fa5e8efa
killmode7 <stanleykaguu7@gmail.com>
2024-11-27 13:44:36 +0300

    Update config.cjs

--- config.cjs
+++ config.cjs
@@ -20,7 +20,7 @@ module.exports = {
   "OWNER_NAME": "MR JANIYA",
   "NEWS_JID": "1234567890@s.whatsapp.net",
   "AUTO_TYPING": false,
-  "AUTO_RECORDING": true,
+  "AUTO_RECORDING": false,
   "AUTO_NEWS": false,
   "OWNER_REACT": false,
   "EMOJI": "🏷"
